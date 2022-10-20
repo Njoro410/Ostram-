0052519dfb6ac357ce04a5c60b7d594ea37e2a41
njoro410 <brian.njoroge@student.moringaschool.com>
2022-10-20 21:02:26 +0300

    added downloads page

---
+++ src/pages/Downloads.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Downloads = () => {
+  return (
+    <div>
+      <div className="p-6 py-12 bg-orange-500">
+        <div className="container mx-auto">
+          <div className="flex flex-col lg:flex-row items-center justify-center">
+            <h2 className="text-center text-4xl md:text-6xl tracking-tighter font-bold">
+              Downloads
+            </h2>
+          </div>
+        </div>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Downloads;
