488f219503f4920f151b649adcd4b80bc4343d5f
njoro410 <brian.njoroge@student.moringaschool.com>
2022-12-04 21:25:03 +0300

    added email functionality for registering

--- src/pages/Membership.jsx
+++ src/pages/Membership.jsx
@@ -1,10 +1,80 @@
-import React from "react";
-
-
+import React, { useRef } from "react";
+import { Helmet } from "react-helmet";
+import ReCAPTCHA from "react-google-recaptcha";
+import Swal from "sweetalert2";
+import emailjs from "@emailjs/browser";
 const Membership = () => {
+  const form = useRef();
+  const full_user_name = useRef(null);
+  const user_phone = useRef(null);
+  const mailRef = useRef(null);
+  const next_of_kin_full_name = useRef(null);
+  const next_relationship = useRef(null);
+  const next_phone = useRef(null);
+  const location = useRef(null);
+  const user_id = useRef(null);
+
+  const captchaRef = useRef();
+
+  const sendEmail = (e) => {
+    emailjs
+      .sendForm(
+        "service_s38ry0n",
+        "template_r1jllsv",
+        form.current,
+        "UfPUtIxYNSr_hAtqq"
+      )
+      .then(
+        (result) => {
+          full_user_name.current.value = "";
+          user_phone.current.value = "";
+          mailRef.current.value="";
+          next_of_kin_full_name.current.value = "";
+          next_relationship.current.value = "";
+          next_phone.current.value = "";
+          location.current.value = "";
+          user_id.current.value = "";
+          Toast.fire({
+            icon: "success",
+            title: "Message sent",
+          });
+          console.log(result.text);
+        },
+        (error) => {
+          Toast.fire({
+            icon: "error",
+            title: "Error, not sent",
+          });
+        }
+      );
+  };
+
+  async function onExpired(value) {
+    await Toast.fire({
+      icon: "error",
+      title: "reCAPTCHA Expired",
+    });
+  }
+
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-right",
+    iconColor: "white",
+    customClass: {
+      popup: "colored-toast",
+    },
+    showConfirmButton: false,
+    timer: 3500,
+    timerProgressBar: true,
+  });
+
   return (
     <div className="dark: dark:bg-gray-800 dark:text-white">
-      <div className="p-6 py-12  dark:bg-orange-500">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Sacco Membership</title>
+      </Helmet>
+      <div className="p-6 py-12  bg-orange-500">
         <div className="container mx-auto">
           <div className="flex flex-col lg:flex-row items-center justify-center">
             <h2 className="text-center text-4xl md:text-6xl tracking-tighter font-bold">
@@ -38,12 +108,8 @@ const Membership = () => {
           </p>
           <ul className="steps steps-vertical">
             <li className="step step-neutral">Fill the registration form</li>
-            <li className="step step-neutral">
-              Avail 2 passport photographs
-            </li>
-            <li className="step step-neutral">
-              Photocopy of your National ID
-            </li>
+            <li className="step step-neutral">Avail 2 passport photographs</li>
+            <li className="step step-neutral">Photocopy of your National ID</li>
             <li className="step step-neutral">
               Pay a Kshs.300/= registration fee
             </li>
@@ -54,21 +120,32 @@ const Membership = () => {
 
         <div className="flex items-center justify-center p-12">
           <div className="mx-auto w-full max-w-[1050px] dark: dark:bg-gray-800">
-            <p className="text-center text-lg underline underline-offset-4">Fill details below</p>
-            <form action="https://formbold.com/s/FORM_ID" method="POST">
+            <p className="text-center text-lg underline underline-offset-4">
+              Fill details below
+            </p>
+            <form
+              id="register"
+              ref={form}
+              onSubmit={(event) => {
+                event.preventDefault();
+                captchaRef.current.execute();
+              }}
+            >
               <div className="mb-5">
                 <label
                   for="name"
-                  className="mb-3 block text-base font-medium text-orange-500"
+                  className="mb-3 block text-base font-medium text-black"
                 >
                   Full Name
                 </label>
                 <input
                   type="text"
-                  name="name"
+                  name="full_user_name"
+                  ref={full_user_name}
+                  required
                   id="name"
                   placeholder="Full Name"
-                  className="w-full rounded-md border border-[#753636] dark:bg-white py-3 px-6 text-base font-medium text-orange-500 outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  className="w-full rounded-md border border-[#753636] dark:bg-white py-3 px-6 text-base font-medium text-black outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
 
@@ -81,9 +158,11 @@ const Membership = () => {
                 </label>
                 <input
                   type="text"
-                  name="name"
-                  id="name"
-                  placeholder="Full Name"
+                  name="user_phone"
+                  required
+                  ref={user_phone}
+                  id="phone_number"
+                  placeholder="Phone Number"
                   className="w-full rounded-md border border-[#753636]  dark:bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
@@ -93,15 +172,42 @@ const Membership = () => {
                   for="name"
                   className="mb-3 block text-base font-medium text-orange-500"
                 >
-                  Location
+                  ID Number
                 </label>
                 <input
                   type="text"
-                  name="name"
-                  id="name"
-                  placeholder="Full Name"
+                  name="id_number"
+                  required
+                  ref={user_id}
+                  id="id_number"
+                  placeholder="ID Number"
                   className="w-full rounded-md border border-[#753636]  dark:bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
+              </div>
+
+              <div className="mb-5">
+                <label
+                  for="name"
+                  className="mb-3 block text-base font-medium text-orange-500"
+                >
+                  Select your location
+                </label>
+                <select
+                  id="countries"
+                  name="location"
+                  required
+                  ref={location}
+                  class="w-full rounded-md border border-[#753636]  dark:bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                >
+                  <option selected>Choose your location</option>
+                  <option value="Kajiado">Kajiado Town</option>
+                  <option value="Ilbissil">Ilbissil</option>
+                  <option value="Isinya">Isinya</option>
+                  <option value="M_46">M-46</option>
+                  <option value="Sajiloni">Sajiloni</option>
+                  <option value="Enkorika">Enkorika</option>
+                  <option value="Other">Other</option>
+                </select>
               </div>
 
               <div className="mb-5">
@@ -112,21 +218,27 @@ const Membership = () => {
                   Email
                 </label>
                 <input
-                  type="text"
-                  name="name"
+                  type="email"
+                  name="email"
+                  ref={mailRef}
                   id="name"
-                  placeholder="Full Name"
+                  placeholder="Enter your email (optional)"
                   className="w-full rounded-md border border-[#753636]  dark:bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
 
-              <div className="mb-6 pt-4">
+              {/* <div className="mb-6 pt-4">
                 <label className="mb-5 block text-xl font-semibold text-orange-500">
                   Upload File
                 </label>
 
                 <div className="mb-8">
-                  <input type="file" name="file" id="file" className="sr-only" />
+                  <input
+                    type="file"
+                    name="file"
+                    id="file"
+                    className="sr-only"
+                  />
                   <label
                     for="file"
                     className="relative flex min-h-[200px] items-center justify-center rounded-md border border-dashed border-[#753636] p-12 text-center"
@@ -144,7 +256,7 @@ const Membership = () => {
                     </div>
                   </label>
                 </div>
-              </div>
+              </div> */}
 
               <div className="mb-5">
                 <label
@@ -155,10 +267,11 @@ const Membership = () => {
                 </label>
                 <input
                   type="text"
-                  name="name"
+                  ref={next_of_kin_full_name}
+                  name="next_of_kin_full_name"
                   id="name"
                   placeholder="Full Name"
-                  className="w-full rounded-md border border-[#753636]  dark:bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  className="w-full rounded-md border border-[#753636]  dark:bg-white py-3 px-6 text-base font-medium text-black outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
 
@@ -171,10 +284,11 @@ const Membership = () => {
                 </label>
                 <input
                   type="text"
-                  name="name"
+                  ref={next_relationship}
+                  name="next_relationship"
                   id="name"
-                  placeholder="Full Name"
-                  className="w-full rounded-md border border-[#753636]  dark:bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  placeholder="Relationship with next of kin"
+                  className="w-full rounded-md border border-[#753636]  dark:bg-white py-3 px-6 text-base font-medium text-black outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
 
@@ -186,11 +300,12 @@ const Membership = () => {
                   Next of Kin number
                 </label>
                 <input
-                  type="text"
-                  name="name"
+                  type="tel"
+                  ref={next_phone}
+                  name="next_phone"
                   id="name"
-                  placeholder="Full Name"
-                  className="w-full rounded-md border border-[#753636]  dark:bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  placeholder="Next of Kin number"
+                  className="w-full rounded-md border border-[#753636]  dark:bg-white py-3 px-6 text-base font-medium text-black outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
 
@@ -202,7 +317,8 @@ const Membership = () => {
                   <div className="flex items-center">
                     <input
                       type="radio"
-                      name="radio1"
+                      value="Individual"
+                      name="account"
                       id="radioButton1"
                       className="h-5 w-5"
                     />
@@ -210,13 +326,14 @@ const Membership = () => {
                       for="radioButton1"
                       className="pl-3 text-base font-medium text-orange-500"
                     >
-                      Individual 
+                      Individual
                     </label>
                   </div>
                   <div className="flex items-center ">
                     <input
                       type="radio"
-                      name="radio1"
+                      value="Joint Group"
+                      name="account"
                       id="radioButton2"
                       className="h-5 w-5"
                     />
@@ -231,7 +348,8 @@ const Membership = () => {
                   <div className="flex items-center">
                     <input
                       type="radio"
-                      name="radio1"
+                      value="Minor"
+                      name="account"
                       id="radioButton2"
                       className="h-5 w-5"
                     />
@@ -244,9 +362,17 @@ const Membership = () => {
                   </div>
                 </div>
               </div>
-
+              <ReCAPTCHA
+                sitekey="6LcMjVQjAAAAAI5onnD9l8UcqPDXu2EA6C-DVswM"
+                size="invisible"
+                theme="light"
+                ref={captchaRef}
+                onChange={sendEmail}
+                onExpired={onExpired}
+                badge="inline"
+              />
               <div>
-                <button className="hover:shadow-form rounded-md  dark:bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none">
+                <button type="submit" className="btn w-full mt-5">
                   Submit
                 </button>
               </div>
